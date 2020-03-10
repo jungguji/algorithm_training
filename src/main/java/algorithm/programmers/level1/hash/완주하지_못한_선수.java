5c649eaae64f8f3990e98521703934fcfd86a5fe
junggu <junggu.ji.dev@gmail.com>
2020-03-10 14:48:40 +0900

    initial commit

---
+++ src/main/java/algorithm/programmers/level1/hash/완주하지_못한_선수.java
@@ -0,0 +1,21 @@
+package algorithm.programmers.level1.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 완주하지_못한_선수 {
+
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        
+        for (int i = 0; i < participant.length; i++) {
+            map.compute(participant[i], (k, v) -> v != null ? null : -1);
+        }
+        
+        for (int i = 0; i < completion.length; i++) { 
+            map.compute(completion[i], (k, v) -> v != null ? null : -1);
+        }
+        
+        return  map.keySet().iterator().next();
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/hash/완주하지_못한_선수TEST.java
@@ -0,0 +1,36 @@
+package algorithm.programmers.level1.hash;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+public class 완주하지_못한_선수TEST {
+
+    private 완주하지_못한_선수 test;
+    
+    @Before
+    public void setup() {
+        test = new 완주하지_못한_선수();
+    }
+    
+    @Test
+    public void solutionTest() {
+        String[] participant = new String[5];
+        participant[0] = "marina";
+        participant[1] = "josipa";
+        participant[2] = "nikola";
+        participant[3] = "nikola";
+        participant[4] = "filipa";
+        
+        
+        String[] completion = new String[participant.length-1];
+        completion[0] = "josipa";
+        completion[1] = "filipa";
+        completion[2] = "marina";
+        completion[3] = "nikola";
+        
+        assertThat(test.solution(participant, completion), is("nikola"));
+    }
+}
