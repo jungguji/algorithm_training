99e423d89d3fa9366f63554bc8666fe99351de1e
junggu <junggu.ji.dev@gmail.com>
2020-03-25 15:27:50 +0900

    type : Feat
    프로그래머스 가운데 글자 가져오기

---
+++ src/main/java/algorithm/programmers/level1/practice/가운데_글자_가져오기.java
@@ -0,0 +1,8 @@
+package algorithm.programmers.level1.practice;
+
+public class 가운데_글자_가져오기 {
+    public String solution(String s) {
+        String answer =  s.substring((s.length()-1)/2, (s.length()/2)+1);
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/가운데_글자_가져오기TEST.java
@@ -0,0 +1,21 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 가운데_글자_가져오기TEST {
+    private 가운데_글자_가져오기 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 가운데_글자_가져오기();
+    }
+    
+    @Test
+    public void solutionTest() {
+        assertEquals("c", test.solution("abcde"));
+        assertEquals("we", test.solution("qwer"));
+    }
+}
