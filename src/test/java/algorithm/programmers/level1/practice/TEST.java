6b1274321fcc431f9e002fda34113a8dfbbb97e9
junggu <junggu.ji.dev@gmail.com>
2020-03-25 15:28:17 +0900

    type : Feat
    공통으로 들어가니깐 인터페이스 상속 받도록 생성

---
+++ src/test/java/algorithm/programmers/level1/practice/TEST.java
@@ -0,0 +1,12 @@
+package algorithm.programmers.level1.practice;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public interface TEST {
+    @BeforeEach
+    public void setup();
+    
+    @Test
+    public void solutionTest();
+}
