7182ac661b8aac61d34494d888a9cde757933a6e
JG.JI <junggu.ji.dev@gmail.com>
2020-03-27 14:54:22 +0900

    type : Feat
    프로그래머스 문자열 내림차순으로 배치하기

---
+++ src/main/java/algorithm/programmers/level1/practice/문자열_내림차수능로_배치하기.java
@@ -0,0 +1,21 @@
+package algorithm.programmers.level1.practice;
+
+import java.util.Arrays;
+
+public class 문자열_내림차수능로_배치하기 {
+
+    public String solution(String s) {
+        char[] array = s.toCharArray();
+        
+        Arrays.sort(array);
+        
+        char[] test = new char[s.length()];
+        int j = 0;
+        for (int i = array.length-1; i >= 0; i--) {
+            test[j++] = array[i];
+        }
+        
+        String answer = String.valueOf(test);
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/문자열_내림차수능로_배치하기TEST.java
@@ -0,0 +1,22 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 문자열_내림차수능로_배치하기TEST implements TEST {
+
+    private 문자열_내림차수능로_배치하기 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 문자열_내림차수능로_배치하기();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals("gfedcbZ", test.solution("Zbcdefg"));
+    }
+
+}
