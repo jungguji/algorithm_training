8c1a6dd0384920931d2b707fe83181f0b46f9498
JG.JI <junggu.ji.dev@gmail.com>
2020-03-29 12:41:30 +0900

    type : Feat
    포르그래머스 시저암호

---
+++ src/test/java/algorithm/programmers/level1/practice/시저암호TEST.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 시저암호TEST implements TEST {
+    
+    private 시저암호 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 시저암호();
+    }
+    
+    @Test
+    public void solutionTest() {
+//        assertEquals("BC", test.solution("AB", 1));
+//        assertEquals("a", test.solution("z", 1));
+        assertEquals("e F d", test.solution("a B z", 4));
+    }
+
+}
