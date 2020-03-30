83702f65c601a9fae3aa7468a79729753c618bc3
junggu <junggu.ji.dev@gmail.com>
2020-03-30 13:44:32 +0900

    type : Feat
    프로그래머스 소수 찾기

---
+++ src/main/java/algorithm/programmers/level1/practice/약수의_합.java
@@ -0,0 +1,49 @@
+package algorithm.programmers.level1.practice;
+
+public class 약수의_합 {
+    
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n/2; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        
+        return answer + n;
+    }
+    
+    public static int solution2(int n) {
+        int answer = 0;
+        int end = n/2;
+        if (n == 1) return 1;
+        if (n == 2) return 3;
+        if (n == 3) return 4;
+        for (int i = 1; i < end; i++) {
+            if (n % i == 0) {
+                end = n / i;
+                answer += i;
+                
+                if (i != end) {
+                    answer += end;
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    public static void main(String args[]) {
+        
+        for (int i = 0; i <= 3000; i++) {
+            int a = solution(i);
+            int b = solution2(i);
+            if (a != b) {
+                System.out.println("틀린 숫자 >> " + i);
+                System.out.println("a >> " + a);
+                System.out.println("b >> " + b);
+            }
+        }
+        
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/약수의_합TEST.java
@@ -0,0 +1,25 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 약수의_합TEST implements TEST {
+    
+    private 약수의_합 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 약수의_합();
+    }
+    
+    @Test
+    public void solutionTest() {
+//        assertEquals(28, test.solution(12));
+//        assertEquals(6, test.solution(5));
+//        assertEquals(9360, test.solution(3000));
+//        assertEquals(48, test.solution(33));
+        assertEquals(1, test.solution2(1));
+    }
+}
