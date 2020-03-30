768cbfd9d68a122713c113b4d7721a1ccc72db5f
junggu <junggu.ji.dev@gmail.com>
2020-03-30 13:45:14 +0900

    type : Feat
    프로그래머스 수박수박수박수?

---
+++ src/main/java/algorithm/programmers/level1/practice/수박수박수박수박수박수.java
@@ -0,0 +1,14 @@
+package algorithm.programmers.level1.practice;
+
+public class 수박수박수박수박수박수 {
+    
+    public String solution(int n) {
+        String[] a = new String[] {"수", "박"};
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(a[i%2]);
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/수박수박수박수박수박수TEST.java
@@ -0,0 +1,23 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 수박수박수박수박수박수TEST implements TEST {
+
+    private 수박수박수박수박수박수 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 수박수박수박수박수박수();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals("수박수", test.solution(3));
+        assertEquals("수박수박", test.solution(4));
+    }
+
+}
