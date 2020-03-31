d0fc23df127b4802b645ebf1db17b8ede2965d73
junggu <junggu.ji.dev@gmail.com>
2020-03-31 17:19:21 +0900

    type : Feat
    프로그래머스 같은 숫자는 싫어

---
+++ src/main/java/algorithm/programmers/level1/practice/같은_숫자는_싫어.java
@@ -0,0 +1,26 @@
+package algorithm.programmers.level1.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 같은_숫자는_싫어 {
+    public int[] solution(int []arr) {
+        
+        List<Integer> list = new ArrayList<Integer>();
+        
+        int pre = 100;
+        for (int i = 0; i < arr.length; i++) {
+            
+            if (arr[i] == pre) {
+                continue;
+            }
+            
+            pre = arr[i];
+            list.add(arr[i]);
+        }
+        
+        
+        int[] answer = list.stream().mapToInt(i->i).toArray();
+        return answer;
+    }
+}
