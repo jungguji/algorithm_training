939b50669e5682cafeab76177ee065910faf1a34
junggu <junggu.ji.dev@gmail.com>
2020-04-02 17:59:17 +0900

    type : Feat
    프로그래머스 자릿수 더하기

---
+++ src/test/java/algorithm/programmers/level1/practice/자릿수_더하기TEST.java
@@ -0,0 +1,23 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 자릿수_더하기TEST implements TEST {
+
+    private 자릿수_더하기 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 자릿수_더하기();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(6, test.solution(123));
+        assertEquals(24, test.solution(987));
+    }
+
+}
