f9195e85f16151149dab38bf092afa54b2561e98
junggu <junggu.ji.dev@gmail.com>
2020-04-06 14:10:34 +0900

    type : Feat
    프로그래머스 최대공약수와 최소공배수

---
+++ src/main/java/algorithm/programmers/level1/practice/최대공약수와_최소공배수.java
@@ -0,0 +1,17 @@
+package algorithm.programmers.level1.practice;
+
+public class 최대공약수와_최소공배수 {
+
+    public int[] solution(int n, int m) {
+        int big = n > m ? n : m;
+        int small = n < m ? n : m;
+        
+        while (m != 0) {
+            int r = n % m;
+            n = m;
+            m = r;
+        }
+        
+        return new int[] {n, n * (big / n) * (small / n)};
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/최대공약수와_최소공배수TEST.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 최대공약수와_최소공배수TEST implements TEST {
+
+    private 최대공약수와_최소공배수 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 최대공약수와_최소공배수();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertArrayEquals(new int[] {3, 12}, test.solution(3, 12));
+        assertArrayEquals(new int[] {1, 10}, test.solution(2, 5));
+        assertArrayEquals(new int[] {1, 15233730}, test.solution(12345, 1234));
+    }
+
+}
