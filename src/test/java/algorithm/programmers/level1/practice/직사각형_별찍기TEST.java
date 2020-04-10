1b54fe7dcc59a1fe895edeafdf45310269b3cd91
junggu <junggu.ji.dev@gmail.com>
2020-04-11 00:44:53 +0900

    type : Feat
    프로그래머스 별 찍기

---
+++ src/test/java/algorithm/programmers/level1/practice/직사각형_별찍기TEST.java
@@ -0,0 +1,21 @@
+package algorithm.programmers.level1.practice;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class 직사각형_별찍기TEST implements TEST {
+
+    private 직사각형_별찍기 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 직사각형_별찍기();
+    }
+
+    @Test
+    public void solutionTest() {
+        
+    }
+
+}
