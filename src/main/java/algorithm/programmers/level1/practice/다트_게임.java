cdc6eb35c5d8806344d920b1f0dfd8c3443be090
junggu <junggu.ji.dev@gmail.com>
2020-04-11 00:44:34 +0900

    type : Feat
    프로그래머스 다트 게임

---
+++ src/main/java/algorithm/programmers/level1/practice/다트_게임.java
@@ -0,0 +1,47 @@
+package algorithm.programmers.level1.practice;
+
+public class 다트_게임 {
+    public int solution(String dartResult) {
+        char[] charArray = dartResult.toCharArray();
+        
+        int[] scores = new int[3];
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+        for (char ch : charArray) {
+            switch (ch) {
+            case 'S':
+                scores[i++] = Integer.parseInt(sb.toString());
+                
+                sb.setLength(0);
+                break;
+            case 'D':
+                int a = Integer.parseInt(sb.toString());
+                scores[i++] = a * a;
+                
+                sb.setLength(0);
+                break;
+            case 'T':
+                int b = Integer.parseInt(sb.toString());
+                scores[i++] = b * b * b;
+                
+                sb.setLength(0);
+                break;
+            case '*':
+                if (i > 1) {
+                    scores[i-2] = scores[i-2] * 2;
+                }
+                scores[i-1] = scores[i-1] * 2;
+                break;
+            case '#':
+                scores[i-1] = scores[i-1] * -1;
+                break;
+            default:
+                sb.append(ch);
+                break;
+            }
+        }
+        
+        int answer = scores[0] + scores[1] + scores[2];
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/다트_게임TEST.java
@@ -0,0 +1,28 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 다트_게임TEST implements TEST {
+
+    private 다트_게임 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 다트_게임();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(37, test.solution("1S2D*3T"));
+        assertEquals(9, test.solution("1D2S#10S"));
+        assertEquals(3, test.solution("1D2S0T"));
+        assertEquals(23, test.solution("1S*2T*3S"));
+        assertEquals(5, test.solution("1D#2S*3S"));
+        assertEquals(-4, test.solution("1T2D3D#"));
+        assertEquals(59, test.solution("1D2S3T*"));
+    }
+
+}
