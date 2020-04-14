5b49951c9f414800f7b0991c03734f20bf2fea9d
junggu <junggu.ji.dev@gmail.com>
2020-04-14 17:01:30 +0900

    type : Feat
    프로그래머스 문자열 내에 p와 y의 개수

---
+++ src/main/java/algorithm/programmers/level1/practice/문자열_내_P와_y의_개수.java
@@ -0,0 +1,22 @@
+package algorithm.programmers.level1.practice;
+
+public class 문자열_내_P와_y의_개수 {
+    
+    boolean solution(String s) {
+        if (!s.contains("p") && !s.contains("y")) {
+            return true;
+        }
+        
+        
+        int is = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ("p".equalsIgnoreCase(String.valueOf(s.charAt(i)))) {
+                ++is;
+            } else if ("y".equalsIgnoreCase(String.valueOf(s.charAt(i)))) {
+                --is;
+            }
+        }
+        
+        return is == 0;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/문자열_내_P와_y의_개수TEST.java
@@ -0,0 +1,23 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 문자열_내_P와_y의_개수TEST implements TEST {
+
+    private 문자열_내_P와_y의_개수 test;
+    @BeforeEach
+    public void setup() {
+        test = new 문자열_내_P와_y의_개수();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertTrue(test.solution("pPoooyY"));
+        assertFalse(test.solution("Pyy"));
+    }
+
+}
