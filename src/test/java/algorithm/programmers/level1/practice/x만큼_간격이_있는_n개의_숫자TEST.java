2b66cce9c1ba736936b2453a556f8de31f8cd95a
junggu <junggu.ji.dev@gmail.com>
2020-04-18 16:21:01 +0900

    type : Feat
    간격이 있는 숫자

---
+++ src/test/java/algorithm/programmers/level1/practice/x만큼_간격이_있는_n개의_숫자TEST.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class x만큼_간격이_있는_n개의_숫자TEST implements TEST {
+
+    private x만큼_간격이_있는_n개의_숫자 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new x만큼_간격이_있는_n개의_숫자();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertArrayEquals(new long[] {2,4,6,8,10}, test.solution(2, 5));
+        assertArrayEquals(new long[] {4,8,12}, test.solution(4, 3));
+        assertArrayEquals(new long[] {-4, -8}, test.solution(-4, 2));
+    }
+
+}
