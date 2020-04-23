87f7b3c0a5754feafa41fbc85c2b6cd0c8dba871
junggu <junggu.ji.dev@gmail.com>
2020-04-23 16:23:59 +0900

    type : Feat
    테스트케이스 추가

--- src/test/java/algorithm/programmers/level2/stackqueue/프린터TEST.java
+++ src/test/java/algorithm/programmers/level2/stackqueue/프린터TEST.java
@@ -18,8 +18,10 @@ public class 프린터TEST implements TEST {
 
     @Test
     public void solutionTest() {
-//        assertEquals(1, test.solution(new int[] {2, 1, 3, 2}, 2));
+        assertEquals(1, test.solution(new int[] {2, 1, 3, 2}, 2));
         assertEquals(5, test.solution(new int[] {1, 1, 9, 1, 1, 1}, 0));
+        assertEquals(6, test.solution(new int[] {2, 2, 2, 1, 3, 4}, 3));
+        assertEquals(2, test.solution(new int[] {1, 2, 3, 2}, 3));
     }
 
 }
