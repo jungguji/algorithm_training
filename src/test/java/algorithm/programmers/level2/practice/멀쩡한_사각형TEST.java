0cee59f0141e457d4f73cdf15295cc50996d145d
junggu <junggu.ji.dev@gmail.com>
2020-04-24 13:41:27 +0900

    type : Feat
    프로그래머스 멀쩡한 사각형

---
+++ src/test/java/algorithm/programmers/level2/practice/멀쩡한_사각형TEST.java
@@ -0,0 +1,27 @@
+package algorithm.programmers.level2.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import algorithm.programmers.level1.practice.TEST;
+
+public class 멀쩡한_사각형TEST implements TEST {
+
+    private 멀쩡한_사각형 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 멀쩡한_사각형();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(80, test.solution(8, 12));
+        assertEquals(12, test.solution(5, 4));
+        assertEquals(40, test.solution(10, 5));
+        assertEquals(352, test.solution(17, 23));
+    }
+
+}
