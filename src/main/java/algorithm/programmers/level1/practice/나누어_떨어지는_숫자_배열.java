af7db85b3f8d0e5e82e2a845516c5d9735bf3ff1
junggu <junggu.ji.dev@gmail.com>
2020-05-01 22:54:41 +0900

    type : feat
    프로그래머스 나누어 떨어지는 배열

---
+++ src/main/java/algorithm/programmers/level1/practice/나누어_떨어지는_숫자_배열.java
@@ -0,0 +1,30 @@
+package algorithm.programmers.level1.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 나누어_떨어지는_숫자_배열 {
+    public int[] solution(int[] arr, int divisor) {
+        Arrays.sort(arr);
+        
+        if (divisor == 1) {
+            return arr;
+        }
+        
+        List<Integer> list = new ArrayList<Integer>();
+        
+        for (int i : arr) {
+            if (i % divisor == 0) {
+                list.add(i);
+            }
+        }
+        
+        if (list.size() == 0) {
+            list.add(-1);
+        }
+        
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/나누어_떨어지는_숫자_배열TEST.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 나누어_떨어지는_숫자_배열TEST implements TEST {
+
+    private 나누어_떨어지는_숫자_배열 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 나누어_떨어지는_숫자_배열();
+    }
+    
+    @Test
+    public void solutionTest() {
+        assertArrayEquals(new int[] {5,10}, test.solution(new int[] {5,9,7,10}, 5));
+        assertArrayEquals(new int[] {1,2,3,36}, test.solution(new int[] {2,36,1,3}, 1));
+        assertArrayEquals(new int[] {-1}, test.solution(new int[] {3,2,6}, 10));
+    }
+
+}
