b67bb572b36db46bcd9ed773150dde7912a83c17
junggu <junggu.ji.dev@gmail.com>
2020-05-02 15:13:21 +0900

    type : Feat
    클라츠 추측

---
+++ src/main/java/algorithm/programmers/level1/practice/콜라츠_추측.java
@@ -0,0 +1,26 @@
+package algorithm.programmers.level1.practice;
+
+public class 콜라츠_추측 {
+
+    public int solution(int num) {
+        int count = 0;
+        
+        long test = num;
+        while (test != 1) {
+            if (test % 2 == 0) {
+                test /= 2;
+            } else {
+                test = (test * 3) + 1;
+            }
+            
+            ++count;
+            
+            if (count == 500) {
+                count = -1;
+                break;
+            }
+        }
+        
+        return count;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level1/practice/콜라츠_추측TEST.java
@@ -0,0 +1,24 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 콜라츠_추측TEST implements TEST {
+
+    private 콜라츠_추측 test;
+    @BeforeEach
+    public void setup() {
+        test = new 콜라츠_추측();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(8, test.solution(6));
+        assertEquals(4, test.solution(16));
+        assertEquals(-1, test.solution(626331));
+        assertEquals(0, test.solution(1));
+    }
+
+}
