ebf4530929e23bc73d242dfbe7394e953ed18c2c
junggu <junggu.ji.dev@gmail.com>
2020-05-03 13:43:37 +0900

    type : Feat
    정수 제곱근 판별

---
+++ src/test/java/algorithm/programmers/level1/practice/정수_제곱근_판별TEST.java
@@ -0,0 +1,23 @@
+package algorithm.programmers.level1.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class 정수_제곱근_판별TEST implements TEST {
+
+    private 정수_제곱근_판별 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 정수_제곱근_판별();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(144, test.solution(121));
+        assertEquals(-1, test.solution(3));
+    }
+
+}
