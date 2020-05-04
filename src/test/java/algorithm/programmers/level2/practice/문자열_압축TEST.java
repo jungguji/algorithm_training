7f8205d9160074185b2075e2baa9189b04f70e4a
junggu <junggu.ji.dev@gmail.com>
2020-05-04 21:01:38 +0900

    type : Feat
    문자열 압축

---
+++ src/test/java/algorithm/programmers/level2/practice/문자열_압축TEST.java
@@ -0,0 +1,31 @@
+package algorithm.programmers.level2.practice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import algorithm.programmers.level1.practice.TEST;
+
+public class 문자열_압축TEST implements TEST {
+
+    private 문자열_압축 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 문자열_압축();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(7, test.solution("aabbaccc"));
+        assertEquals(9, test.solution("ababcdcdababcdcd"));
+        assertEquals(8, test.solution("abcabcdede"));
+        assertEquals(14, test.solution("abcabcabcabcdededededede"));
+        assertEquals(17, test.solution("xababcdcdababcdcd"));
+        assertEquals(1, test.solution("a"));
+        assertEquals(2, test.solution("aaaaa"));
+        assertEquals(3, test.solution("aaaaaaaaaa"));
+    }
+
+}
