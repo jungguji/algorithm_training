819f32c6a3bb18f10224d08e126803e59b0d5e52
junggu <junggu.ji.dev@gmail.com>
2020-05-11 19:51:22 +0900

    type : Feat
    비밀지도

--- src/main/java/algorithm/programmers/level1/practice/비밀지도.java
+++ src/main/java/algorithm/programmers/level1/practice/비밀지도.java
@@ -2,40 +2,29 @@ package algorithm.programmers.level1.practice;
 
 public class 비밀지도 {
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] binary1 = getBinary(n, arr1);
-        String[] binary2 = getBinary(n, arr2);
+        String[] binary = getBinary(n, arr1, arr2);
         
         String[] answer = new String[n];
-        for (int i = 0; i < n; i++) {
-            char[] ch1 = binary1[i].toCharArray();
-            char[] ch2 = binary2[i].toCharArray();
-            StringBuilder sb = new StringBuilder();
-            
-            for (int j = 0; j < ch1.length; j++) {
-                if ((ch1[j] == '0') && (ch2[j] == '0')) {
-                    sb.append(" ");
-                } else {
-                    sb.append("#");
-                }
-            }
-            
-            answer[i] = sb.toString();
+        int i = 0;
+        for (String str : binary) {
+            str = str.replaceAll("0", " ");
+            str = str.replaceAll("1", "#");
+            answer[i++] = str;
         }
-        
         return answer;
     }
     
-    private String[] getBinary(int length, int[] arr) {
+    private String[] getBinary(int length, int[] arr1, int[] arr2) {
         String[] binary = new String[length];
         
-        for (int i = 0; i < arr.length; i++) {
-            String str = Integer.toBinaryString(arr[i]);
-            String s = "";
+        for (int i = 0; i < arr1.length; i++) {
+            String str = Integer.toBinaryString(arr1[i] | arr2[i]);
+            StringBuilder sb = new StringBuilder();
             for (int j = 0; j < length - str.length(); j++) {
-                s += "0";
+                sb.append("0");
             }
             
-            binary[i] = s + str;
+            binary[i] = sb.append(str).toString();
         }
         
         return binary;
