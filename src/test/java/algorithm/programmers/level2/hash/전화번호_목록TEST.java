7e5776ce8af8e7b6b79dccdbb4226b3bdf4554a9
junggu <junggu.ji.dev@gmail.com>
2020-05-14 17:36:32 +0900

    type : feat
    전화번호 목록

---
+++ src/test/java/algorithm/programmers/level2/hash/전화번호_목록TEST.java
@@ -0,0 +1,26 @@
+package algorithm.programmers.level2.hash;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import algorithm.programmers.level1.practice.TEST;
+
+public class 전화번호_목록TEST implements TEST {
+
+    private 전화번호_목록 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 전화번호_목록();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertFalse(test.solution(new String[] {"119", "97674223", "1195524421"} ));
+        assertTrue(test.solution(new String[] {"123","456","789"} ));
+        assertFalse(test.solution(new String[] {"12","123","1235","567","88"} ));
+    }
+
+}
