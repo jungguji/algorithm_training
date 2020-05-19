52b9b55ccfb4a7ae8b206134b3d145dad413ff9e
junggu <junggu.ji.dev@gmail.com>
2020-05-19 14:45:25 +0900

    type : Feat
    구명보트

---
+++ src/main/java/algorithm/programmers/level2/greedy/구명보트.java
@@ -0,0 +1,26 @@
+package algorithm.programmers.level2.greedy;
+
+import java.util.Arrays;
+
+public class 구명보트 {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        int answer = 0;
+        while (left <= right) {
+            int sum = people[left] + people[right];
+            
+            if (sum <= limit) {
+                ++left;
+            }
+            
+            ++answer;
+            --right;
+        }
+        
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level2/greedy/구명보트TEST.java
@@ -0,0 +1,29 @@
+package algorithm.programmers.level2.greedy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import algorithm.programmers.level1.practice.TEST;
+
+public class 구명보트TEST implements TEST {
+
+    private 구명보트 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 구명보트();
+    }
+
+    @Test
+    public void solutionTest() {
+        assertEquals(3, test.solution(new int[] {70, 50, 80, 50}, 100));
+        assertEquals(3, test.solution(new int[] {70, 80, 50}, 100));
+        assertEquals(2, test.solution(new int[] {40, 40, 80}, 160));
+        assertEquals(2, test.solution(new int[] {20, 50, 50, 80}, 100));
+        assertEquals(5, test.solution(new int[] {40,50,60,70,80,90}, 100));
+        assertEquals(2, test.solution(new int[] {40,40,40}, 100));
+    }
+
+}
