bd30b85fc4b6f3f578b9d9349285056d4120fe56
junggu <junggu.ji.dev@gmail.com>
2020-05-23 12:28:59 +0900

    type : Feat
    주석 해제

--- src/test/java/algorithm/baekjoon/string/단어의_개수TEST.java
+++ src/test/java/algorithm/baekjoon/string/단어의_개수TEST.java
@@ -19,10 +19,10 @@ public class 단어의_개수TEST implements TEST {
     @SuppressWarnings("static-access")
     @Test
     public void solutionTest() {
-//        assertEquals(6, test.solution("The Curious Case of Benjamin Button"));
-//        assertEquals(3, test.solution(" Mazatneunde Wae Teullyeoyo"));
-//        assertEquals(2, test.solution("Teullinika Teullyeotzi "));
-//        assertEquals(7, test.solution(" a b c d e f g "));
+        assertEquals(6, test.solution("The Curious Case of Benjamin Button"));
+        assertEquals(3, test.solution(" Mazatneunde Wae Teullyeoyo"));
+        assertEquals(2, test.solution("Teullinika Teullyeotzi "));
+        assertEquals(7, test.solution(" a b c d e f g "));
         assertEquals(0, test.solution(" "));
     }
 
