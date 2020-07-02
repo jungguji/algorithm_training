9bf66076c33a5cfe906b3f55b93f617c0c79d324
junggu <junggu.ji.dev@gmail.com>
2020-07-02 21:55:27 +0900

    type : Feat
    백준 고양이

---
+++ src/main/java/algorithm/baekjoon/inoutput/고양이.java
@@ -0,0 +1,11 @@
+package algorithm.baekjoon.inoutput;
+
+public class 고양이 {
+
+    public static void main(String[] args) {
+       System.out.println("\\    /\\ ");
+       System.out.println(" )  ( ')");
+       System.out.println("(  /  ) ");
+       System.out.println(" \\(__)| ");
+    }
+}
