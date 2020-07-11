ba85b35c1d58257a7fbe6cb1ee83ae9970a4ee8b
junggu <junggu.ji.dev@gmail.com>
2020-07-11 20:59:47 +0900

    type : Refactor
    함수로 추출

--- src/main/java/algorithm/programmers/level2/greedy/조이스틱.java
+++ src/main/java/algorithm/programmers/level2/greedy/조이스틱.java
@@ -10,70 +10,93 @@ public class 조이스틱 {
     public int solution(String name) {
         char[] toChar = name.toCharArray();
         
-        int answer = 0;
-        for (int i = 0; i < toChar.length; i++) {
-            char c = toChar[i];
-            
-            int charASCII = c;
-            int upDownCurour = charASCII - A_ASCII;
-            if (upDownCurour > 12) {
-                upDownCurour = 26 - upDownCurour;
-            }
-            
-            answer += upDownCurour;
-        }
+        int answer = getUpDownCount(toChar);
         
-        int[] serialAIndex = getLongestSerialAIndex(toChar);
+        int[] serialIndex = getLongestSerialIndex(toChar);
         
-        int leftRightCursur = 0;
+        int leftRightCursur = getLeftRightCount(serialIndex[0], serialIndex[1], toChar.length);
         
-        if (serialAIndex[0] == 0 && serialAIndex[1] == 0) {
-            leftRightCursur = toChar.length - 1;
-        } else {
-            int left = serialAIndex[0] == 0 ? 0 : serialAIndex[0] - 1;
-            int right = toChar.length - serialAIndex[1];
-            
-            leftRightCursur = left > right ? (right * 2) + left : (left * 2) + right;
-            leftRightCursur = leftRightCursur > toChar.length - 1 ? toChar.length - 1 : leftRightCursur;
-        }
-        
-        answer += leftRightCursur ;
+        answer += leftRightCursur;
         
         return answer;
     }
-    
-    private int[] getLongestSerialAIndex(char[] array) {
+
+    private int getUpDownCount(char[] names) {
         int count = 0;
-        List<Serial> list = new ArrayList<Serial>();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == 'A') {
-                if (count == 0) {
-                    list.add(new Serial(i, 1));
-                } else {
-                    list.get(list.size()-1).count = (count+1);
-                }
-                
-                count++;
-            } else {
-                count = 0;
+        for (char c : names) {
+            int upDownCurour = c - A_ASCII;
+            if (upDownCurour > 12) {
+                upDownCurour = 26 - upDownCurour;
             }
+
+            count += upDownCurour;
         }
+
+        return count;
+    }
+    
+    private int[] getLongestSerialIndex(char[] array) {
+        List<Serial> list = getSerialList(array);
         
-        int max = 0;
-        Serial serial = new Serial();
-        
-        for (Serial s : list) {
-            if (max <= s.count) {
-                max = s.count;
-                serial = s;
-            }
-        }
+        Serial serial = getMaxCountSerial(list);
         
         int[] result = new int[2];
         result[0] = serial.index;
         result[1] = serial.index + serial.count;
         
         return result;
+    }
+
+    private List<Serial> getSerialList(char[] array) {
+        int count = 0;
+        List<Serial> list = new ArrayList<Serial>();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 'A') {
+                count = 0;
+                continue;
+            }
+
+            if (count == 0) {
+                list.add(new Serial(i, 1));
+            } else {
+                list.get(list.size()-1).count = (count+1);
+            }
+
+            count++;
+        }
+
+        return list;
+    }
+
+    private Serial getMaxCountSerial(List<Serial> list) {
+        int max = 0;
+        Serial serial = new Serial();
+
+        for (Serial s : list) {
+            if (max <= s.count) {
+                max = s.count;
+                serial = s;
+            }
+        }
+
+        return serial;
+    }
+
+    private int getLeftRightCount(int serialIndex, int serialCount, int nameLength) {
+        int leftRightCursur = 0;
+
+        if (serialIndex == 0 && serialCount == 0) {
+            leftRightCursur = nameLength - 1;
+        } else {
+            int left = serialIndex == 0 ? 0 : serialIndex - 1;
+            int right = nameLength - serialCount;
+
+            leftRightCursur = left > right ? (right * 2) + left : (left * 2) + right;
+            leftRightCursur = leftRightCursur > nameLength - 1 ? nameLength - 1 : leftRightCursur;
+        }
+
+        return leftRightCursur;
     }
     
     class Serial {
