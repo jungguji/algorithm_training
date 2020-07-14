cf164b5deb921b3cb39e68803924450510055ac7
junggu <junggu.ji.dev@gmail.com>
2020-07-14 10:11:55 +0900

    type : Test
    테스트 케이스 수정

--- src/test/java/algorithm/programmers/level2/greedy/큰_수_만들기TEST.java
+++ src/test/java/algorithm/programmers/level2/greedy/큰_수_만들기TEST.java
@@ -1,11 +1,10 @@
 package algorithm.programmers.level2.greedy;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import algorithm.programmers.level1.practice.TEST;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import algorithm.programmers.level1.practice.TEST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class 큰_수_만들기TEST implements TEST {
 
@@ -24,6 +23,6 @@ public class 큰_수_만들기TEST implements TEST {
         assertEquals("3234", test.solution("1231234", 3));
         assertEquals("775841", test.solution("4177252841", 4));
         assertEquals("9", test.solution("9999999999", 9));
-        assertEquals("93231357719", test.solution("9312131357719", 2));
+        assertEquals("93231357719", test.solution("9312131357719", 5));
     }
 }
