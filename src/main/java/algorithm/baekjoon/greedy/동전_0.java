63f8c1cf29cc62a32ee6fbe906abc76cc72b380a
junggu <junggu.ji.dev@gmail.com>
2020-07-23 21:24:39 +0900

    type : Feat
    백준 동전

---
+++ src/main/java/algorithm/baekjoon/greedy/동전_0.java
@@ -0,0 +1,53 @@
+package algorithm.baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 동전_0 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String coinAndMoney = br.readLine();
+        
+        String[] coinAndMoneyArray = coinAndMoney.split(" ");
+        
+        int coinAmount = Integer.parseInt(coinAndMoneyArray[0]);
+        long money = Long.parseLong(coinAndMoneyArray[1]);
+        
+        long[] coinCategory = getCoincategory(coinAmount, br);
+
+        long answer = solution(coinAmount, money, coinCategory);
+        
+        System.out.println(answer);
+    }
+    
+    public static long[] getCoincategory(int coinAmount, BufferedReader br) throws IOException {
+        long[] result = new long[coinAmount];
+        for (int i = 0; i < coinAmount; i++) {
+            
+            result[i] = Integer.parseInt(br.readLine());
+        }
+        
+        return result;
+    }
+    
+    public static long solution(int n, long money, long[] array) {
+        long answer = 0;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (array[i] > money) {
+                continue;
+            }
+            
+            answer += (money / array[i]);
+            money %= array[i];
+            
+            if (money == 0) {
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/baekjoon/greedy/동전_0TEST.java
@@ -0,0 +1,45 @@
+package algorithm.baekjoon.greedy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import algorithm.programmers.level1.practice.TEST;
+
+public class 동전_0TEST implements TEST {
+
+    private 동전_0 test;
+    
+    @BeforeEach
+    public void setup() {
+        test = new 동전_0();
+    }
+
+    @Test
+    public void solutionTest() {
+        long[] array = new long[] {1,5,10,50,100,500,1000,5000,10000,50000};
+        assertEquals(6, test.solution(10, 4200, array));
+        assertEquals(12, test.solution(10, 4790, array));
+        assertEquals(1, test.solution(10, 50000, array));
+        assertEquals(2000, test.solution(10, 100000000, array));
+        
+        array = new long[] {1};
+        assertEquals(2, test.solution(1, 2, array));
+        assertEquals(1, test.solution(1, 1, array));
+        assertEquals(100000000, test.solution(1, 100000000, array));
+        
+        array = new long[] {1, 5};
+        assertEquals(1, test.solution(2, 5, array));
+        
+        array = new long[] {1, 3};
+        assertEquals(2, test.solution(2, 4, array));
+        
+        array = new long[] {1, 100};
+        assertEquals(1, test.solution(2, 100, array));
+        
+        array = new long[] {5000};
+        assertEquals(1, test.solution(1, 5000, array));
+    }
+
+}
