f1b7a57bd60d60972fd2a00138da27f818c66ded
JG.JI <junggu.ji.dev@gmail.com>
2020-07-28 19:05:52 +0900

    type : temp
    자리 이동

---
+++ src/main/java/algorithm/programmers/level2/practice/프렌즈4블록.java
@@ -0,0 +1,114 @@
+package algorithm.programmers.level2.practice;
+
+public class 프렌즈4블록 {
+    private static final int ASCII = 32;
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        char[][] charBoard = convertStringArrayToCharArray(m, n, board);
+
+        boolean isEnd = false;
+        while (!isEnd) {
+            isEnd = true;
+            isEnd = box(charBoard);
+
+            if (isEnd) {
+                break;
+            }
+
+            answer += delete(charBoard);
+        }
+
+        return answer;
+    }
+
+    public char[][] convertStringArrayToCharArray(int m, int n, String[] board) {
+        char[][] charBoard = new char[m][n];
+
+        int i = 0;
+        for (String str : board) {
+            char[] toChar = str.toCharArray();
+            charBoard[i] = toChar;
+            ++i;
+        }
+
+        return charBoard;
+    }
+
+    public char toLowerCase(char value) {
+        return (char) ((value >= 65 && value <= 90) ? (value + ASCII) : value);
+    }
+
+    public boolean box(char[][] charBoard) {
+        boolean isEnd = true;
+
+        for (int i = 0; i < charBoard.length; i++) {
+            for (int j = 0; j < charBoard[i].length; j++) {
+                if (validation(i, j, charBoard)) {
+                    char value = toLowerCase(charBoard[i][j]);
+                    charBoard[i][j] = value;
+                    charBoard[i][j+1] = value;
+                    charBoard[i+1][j] = value;
+                    charBoard[i+1][j+1] = value;
+
+                    isEnd = false;
+                }
+            }
+        }
+
+        return isEnd;
+    }
+
+    private boolean validation(int i, int j, char[][] charBoard) {
+        if ((i + 1) == charBoard.length || (j + 1) == charBoard[0].length) {
+            return false;
+        }
+        char position = toLowerCase(charBoard[i][j]);
+
+        if (position == ' ') {
+            return false;
+        }
+
+        char moveX = toLowerCase(charBoard[i][j+1]);
+        char moveY = toLowerCase(charBoard[i+1][j]);
+        char moveXY = toLowerCase(charBoard[i+1][j+1]);
+
+        if (position != moveX) {
+            return false;
+        }
+
+        if (position != moveY) {
+            return false;
+        }
+
+        if (position != moveXY) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private int delete(char[][] charBoard) {
+        int answer = 0;
+
+        for (int i = 0; i < charBoard.length; i++) {
+            for (int j = 0; j < charBoard[i].length; j++) {
+                if (charBoard[i][j] == ' ') {
+                    continue;
+                }
+
+                if (charBoard[i][j] >= 97 && charBoard[i][j] <= 122) {
+                    if (i != 0) {
+                        for (int k = i; k > 0; k--) {
+                            charBoard[k][j] = charBoard[k-1][j];
+                            charBoard[k-1][j] = ' ';
+                        }
+                    }
+
+                    ++answer;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/algorithm/programmers/level2/practice/프렌즈4블록Test.java
@@ -0,0 +1,52 @@
+package algorithm.programmers.level2.practice;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class 프렌즈4블록Test {
+
+    private 프렌즈4블록 test;
+
+    @BeforeEach
+    void setUp() {
+        test = new 프렌즈4블록();
+    }
+
+    @Test
+    void solution() {
+        assertEquals(14, test.solution(4,5, new String[] {"CCBDE", "AAADE", "AAABF", "CCBBF"}));
+        assertEquals(15, test.solution(6,6, new String[] {"TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"}));
+        assertEquals(14, test.solution(4,5, new String[] {"AAAAA","AUUUA","AUUAA","AAAAA"}));
+        assertEquals(4, test.solution(2,2, new String[] {"AA","AA"}));
+        assertEquals(0, test.solution(2,2, new String[] {"AA","AB"}));
+        assertEquals(4, test.solution(3,2, new String[] {"AA","AA", "AB"}));
+        assertEquals(8, test.solution(4,2, new String[] {"CC","AA", "AA", "CC"}));
+        assertEquals(12, test.solution(6,2, new String[] {"DD", "CC", "AA", "AA", "CC", "DD"}));
+        assertEquals(8, test.solution(8,2, new String[] {"FF", "AA", "CC", "AA", "AA", "CC", "DD", "FF"}));
+        assertEquals(8, test.solution(6,2, new String[] {"AA", "AA", "CC", "AA", "AA", "DD"}));
+        assertEquals(8, test.solution(4,4, new String[] {"ABCD", "BACE", "BCDD", "BCDD"}));
+        assertEquals(27, test.solution(8,9, new String[] {"ABCDADFDA", "ABDFQWERF", "WKDNFNRIT", "AKAKWODCJ", "AKAKWODCJ", "KKKKKKKKK", "KKKKKKKKK", "KKKKKKKKK"}));
+        assertEquals(15, test.solution(4,5, new String[] {"AAAAA", "AAAAU", "AAAUU", "UUUUU"}));
+    }
+
+    @Test
+    void convertStringArrayToCharArray() {
+        assertArrayEquals(new char[][] {{'C','C','B','D','E'}, {'A','A','A','D','E'}, {'A','A','A','B','F'}, {'C','C','B','B','F'}}, test.convertStringArrayToCharArray(4, 5, new String[] {"CCBDE", "AAADE", "AAABF", "CCBBF"}));
+    }
+
+    @Test
+    void toLowerCase() {
+        assertEquals('a', test.toLowerCase('a'));
+        assertEquals('a', test.toLowerCase('A'));
+    }
+
+    @Test
+    void box() {
+        //given
+        char[][] given = test.convertStringArrayToCharArray(4,5, new String[] {"CCBDE", "AAADE", "AAABF", "CCBBF"});
+
+        assertFalse(test.box(given));
+    }
+}
