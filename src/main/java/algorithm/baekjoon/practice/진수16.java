eff6a60c780cf19d520df9ccba08d92b35e25559
junggu <junggu.ji.dev@gmail.com>
2020-09-01 21:37:45 +0900

    type : Feat
    16진수

---
+++ src/main/java/algorithm/baekjoon/practice/진수16.java
@@ -0,0 +1,20 @@
+package algorithm.baekjoon.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class 진수16 {
+    public static void main(String[] args) throws IOException {
+        String input  = getInputData(System.in);
+
+        System.out.println(Integer.parseInt(input, 16));
+    }
+
+    public static String getInputData(InputStream in) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            return br.readLine();
+        }
+    }
+}
