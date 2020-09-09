083d797d78531fec6430a39414a60d4d55163b91
JG.JI <junggu.ji.dev@gmail.com>
2020-09-09 15:35:14 +0900

    type : init

---
+++ src/main/java/algorithm/baekjoon/class2/stackqueue/프린터_큐.java
@@ -0,0 +1,78 @@
+package algorithm.baekjoon.class2.stackqueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class 프린터_큐 {
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int amount = Integer.parseInt(br.readLine());
+
+            StringBuilder sb = new StringBuilder();
+            while (amount-- > 0) {
+                int[] nm = convertStringArrayToIntegerArray(br.readLine().split(" "));
+                int[] importance = convertStringArrayToIntegerArray(br.readLine().split(" "));
+
+                sb.append(solution(nm[0], nm[1], importance)).append("\n");
+            }
+
+            System.out.println(sb);
+        }
+    }
+
+    private static int[] convertStringArrayToIntegerArray(String[] args) {
+        int[] array = new int[args.length];
+        int i = 0;
+        for (String str : args) {
+            array[i++] = Integer.parseInt(str);
+        }
+
+        return array;
+    }
+
+    public static int solution(int n, int m, int[] importance) {
+        LinkedList<Integer[]> printer = initPrinter(n, importance);
+
+        int count = 0;
+        while (!printer.isEmpty()) {
+            Integer[] print = printer.poll();
+            int sizeAfterPop = printer.size();
+            int sizeAfterOffer = printer.size();
+
+            --m;
+            ++count;
+
+            for (int i = 0; i < printer.size(); i++) {
+                if (printer.get(i)[1] > print[1]) {
+                    printer.offer(print);
+                    sizeAfterOffer = printer.size();
+
+                    --count;
+                    ++m;
+
+                    m = (m == 0) ? printer.size() - 1 : --m;
+                    break;
+                }
+            }
+
+
+            if (m == 0 && sizeAfterPop == sizeAfterOffer) {
+                break;
+            }
+        }
+
+        return count;
+    }
+
+    private static LinkedList<Integer[]> initPrinter(int n, int[] importance) {
+        LinkedList<Integer[]> printer = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            printer.offer(new Integer[] {i, importance[i]});
+        }
+
+        return printer;
+    }
+}
