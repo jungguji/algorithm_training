96a5731f6670c51f855e65156b2d5173758eb8a4
junggu.ji.dev@gmail.com <Giwndrn(!446>
2020-10-31 20:25:35 +0900

    type : refactor
    junit5로 변경

--- src/test/java/algorithm/programmers/level1/practice/하샤드_수TEST.java
+++ src/test/java/algorithm/programmers/level1/practice/하샤드_수TEST.java
@@ -1,6 +1,7 @@
 package algorithm.programmers.level1.practice;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
