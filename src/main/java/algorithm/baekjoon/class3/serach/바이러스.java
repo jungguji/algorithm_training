93ff985e7ccb3da6b0bd47137d0c9cf2e872a79e
jungguji <elementbyid@gmail.com>
2020-11-11 09:56:07 +0000

    type : test repl.it test

--- src/main/java/algorithm/baekjoon/class3/serach/바이러스.java
+++ src/main/java/algorithm/baekjoon/class3/serach/바이러스.java
@@ -1,5 +1,8 @@
 package algorithm.baekjoon.class3.serach;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class 바이러스 {
    public static void main(String[] args) {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
