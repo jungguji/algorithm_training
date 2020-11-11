36c00632353795420e08d74491afb51fd6497e59
jungguji <elementbyid@gmail.com>
2020-11-11 09:40:35 +0000

    type : test
    repl.it user.email test

--- src/main/java/algorithm/baekjoon/class3/serach/바이러스.java
+++ src/main/java/algorithm/baekjoon/class3/serach/바이러스.java
@@ -3,5 +3,8 @@ package
 class 바이러스 {
    public static void main(String[] args) {
        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       
+       br.readLine();
+       int n = Integer.parseInt(br.readLine());
    }
 }
