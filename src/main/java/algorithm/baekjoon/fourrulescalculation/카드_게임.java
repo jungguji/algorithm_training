26f6c52d3af49912350df476eee602b9fd4bb7b0
junggu.ji <junggu.ji.dev@gmail.com>
2020-12-16 15:26:55 +0900

    type: feat 카드게임

---
+++ src/main/java/algorithm/baekjoon/fourrulescalculation/카드_게임.java
@@ -0,0 +1,20 @@
+package algorithm.baekjoon.fourrulescalculation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 카드_게임 {
+    private static int N = 5;
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        while (N-- > 0) {
+            sum += Integer.parseInt(br.readLine());
+        }
+        
+        System.out.println(sum);
+    }
+}
