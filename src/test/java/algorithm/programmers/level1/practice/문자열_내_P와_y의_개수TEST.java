d971ea77eab3c69164b5c3169a858400acdea8c7
jungguji <junggu.ji.dev@gmail.com>
2021-02-27 17:39:09 +0900

    fix : junit5로 변경

--- src/test/java/algorithm/programmers/level1/practice/문자열_내_P와_y의_개수TEST.java
+++ src/test/java/algorithm/programmers/level1/practice/문자열_내_P와_y의_개수TEST.java
@@ -1,7 +1,7 @@
 package algorithm.programmers.level1.practice;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
