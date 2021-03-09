3372b614eeb07b28986ca0a7084cd56b3ee0447b
jungguji <junggu.ji.dev@gmail.com>
2021-03-09 22:22:02 +0900

    refactor : junit 5로 변경

--- src/test/java/algorithm/programmers/level1/practice/실패율TEST.java
+++ src/test/java/algorithm/programmers/level1/practice/실패율TEST.java
@@ -1,8 +1,7 @@
 package algorithm.programmers.level1.practice;
 
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.HashMap;
