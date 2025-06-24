5087ccb451b2962849700c1381176eda1ede0491
JG.JI <junggu.ji.dev@gmail.com>
2025-06-24 13:56:27 +0900

    feat: 프로그래머스 로또의 최고 순위와 최저순위

---
+++ src/main/java/algorithm/programmers/로또의최고순위와최저순위.java
@@ -0,0 +1,48 @@
+package algorithm.programmers;
+
+public class 로또의최고순위와최저순위 {
+
+    public int[] solution(int[] lottos, int[] win_nums) {
+
+        int 맞은수 = 0;
+        int 가려진수 = 0;
+        for (int i = 0; i < lottos.length; ++i) {
+            if (lottos[i] == 0) {
+                ++가려진수;
+                continue;
+            }
+
+            for (int j = 0; j < lottos.length; ++j) {
+                if (lottos[i] == win_nums[j]) {
+                    ++맞은수;
+                    break;
+                }
+            }
+        }
+
+        return new int[]{가져오기_등수(맞은수 + 가려진수), 가져오기_등수(맞은수)};
+    }
+
+    private static int 가져오기_등수(int 맞은수) {
+        int 등수 = 6;
+        switch (맞은수) {
+            case 6 :
+                등수 = 1;
+                break;
+            case 5:
+                등수 = 2;
+                break;
+            case 4:
+                등수 = 3;
+                break;
+            case 3:
+                등수 = 4;
+                break;
+            case 2:
+                등수 = 5;
+                break;
+        }
+
+        return 등수;
+    }
+}
