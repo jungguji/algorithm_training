4f692c29bd19b5e679ec5f6b3b44863ebe45fbf2
JG.JI <junggu.ji.dev@gmail.com>
2025-06-24 14:29:03 +0900

    feat: 백준 돌게임

---
+++ src/main/java/algorithm/baekjoon/avatar/silver/돌게임.java
@@ -0,0 +1,15 @@
+package algorithm.baekjoon.avatar.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 돌게임 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+
+            System.out.println(N % 2 == 1 ? "SK" : "CY");
+        }
+    }
+}
