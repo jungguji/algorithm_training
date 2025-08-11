357f540a544cd0bbf523421b55fa18043285c3b0
중구 <junggu@jung-guui-MacBookAir.local>
2025-08-11 12:30:46 +0900

    feat: 진법변환 복습

---
+++ src/main/java/algorithm/baekjoon/math/bronze/진법변환_복습_0.java
@@ -0,0 +1,29 @@
+package algorithm.baekjoon.math.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 진법변환_복습_0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] NB = br.readLine().split(" ");
+
+        // 수 * 진법^자리수
+        char[] N = NB[0].toCharArray();
+        int B = Integer.parseInt(NB[1]);
+
+        int sum = 0;
+        int exponents = 0;
+        for (int i = N.length-1; i >= 0; --i) {
+            int value = N[i] - '0';
+            if ('A' <= N[i] && N[i] <= 'Z') {
+                value = N[i] - 55;
+            }
+
+            sum += value * (int)Math.pow(B, exponents++);
+        }
+
+        System.out.println(sum);
+    }
+}
