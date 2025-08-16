6cdd03b6b2404a01027fb23f0c1af83511206dc6
JG.JI <junggu.ji.dev@gmail.com>
2025-08-16 16:08:23 +0900

    feat: 백준

---
+++ src/main/java/algorithm/baekjoon/math/수학은체육과목입니다.java
@@ -0,0 +1,14 @@
+package algorithm.baekjoon.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 수학은체육과목입니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+        System.out.println(n * 4);
+
+    }
+}
