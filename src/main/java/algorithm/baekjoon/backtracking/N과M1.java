1d20c0ec03682a80703dfe53358a4b9acb1672c4
junggu.ji <junggu.ji.dev@gmail.com>
2025-10-07 16:41:14 +0900

    feat: 백트래킹 시작

---
+++ src/main/java/algorithm/baekjoon/backtracking/N과M1.java
@@ -0,0 +1,58 @@
+package algorithm.baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class N과M1 {
+    private static boolean[] visited;
+    private static int N;
+    private static int M;
+    public static int[] arr;
+
+    private static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] NM = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        N = NM[0];
+        M = NM[1];
+
+        arr = new int[M];
+
+        visited = new boolean[N+1];
+
+        dfs(0);
+
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; ++i) {
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            arr[depth] = i ;
+
+            dfs(depth+1);
+
+            visited[i] = false;
+        }
+    }
+}
